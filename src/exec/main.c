dc32bec130ae4651e3ce1848df83732ccc040c90
Hsin-yu Liu <hsliu@z4r12p3.42lyon.fr>
2023-03-21 14:46:10 +0100

    MN-18 a simple main

--- src/exec/main.c
+++ src/exec/main.c
@@ -6,7 +6,7 @@
 /*   By: hsliu <hsliu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 11:16:24 by hsliu             #+#    #+#             */
-/*   Updated: 2023/03/21 13:04:49 by hsliu            ###   ########.fr       */
+/*   Updated: 2023/03/21 14:45:02 by hsliu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,19 @@
 #include "world.h"
 #include "camera.h"
 #include "mlx.h"
-
+#include "exec.h"
 /*
     for testing
 */
 int main(void)
 {
     t_minirt	minirt;
+    
+    if (ft_init_mlx(&minirt))
+    {
+        write(2, "init fails\n", 11);
+    }
+    ft_set_hook(minirt.win)
+    mlx_loop(minirt.mlx);
+    
 }
