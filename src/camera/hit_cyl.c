e94dfff6fe010c0fd3a215e76fabfa1239d220f2
Hsin-yu Liu <hsliu@z4r9p1.42lyon.fr>
2023-03-29 13:38:38 +0200

    rm comment

--- src/camera/hit_cyl.c
+++ src/camera/hit_cyl.c
@@ -6,7 +6,7 @@
 /*   By: hsliu <hsliu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 13:21:23 by hsliu             #+#    #+#             */
-/*   Updated: 2023/03/29 13:23:03 by hsliu            ###   ########.fr       */
+/*   Updated: 2023/03/29 13:36:16 by hsliu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,6 @@ bool	ft_hit_cyl(t_cylinder cyl, t_ray ray, double dist_max, t_record *rec)
 		ft_copy_rec(rec, temp + 1);
 		return (TRUE);
 	}
-	//return (TRUE); //TEST
 	return (FALSE);
 }
 
