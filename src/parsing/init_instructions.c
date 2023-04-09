ee29e63ba9b04e4224ab4171e2b3f89921ab3e50
bl000m <mathiapagani@gmail.com>
2023-04-09 15:33:08 +0000

    parsing objects in linked list working well even if there is more than one object of same type

--- src/parsing/init_instructions.c
+++ src/parsing/init_instructions.c
@@ -6,7 +6,7 @@
 /*   By: mathia <mathia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 09:36:05 by mathia            #+#    #+#             */
-/*   Updated: 2023/04/08 20:00:09 by mathia           ###   ########.fr       */
+/*   Updated: 2023/04/09 15:29:23 by mathia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void    initialize_plane(t_minirt *minirt, char **tokens)
     new_plane->pos = get_instruction(tokens, 1);
     new_plane->dir = get_instruction(tokens, 2);
     new_plane->color = get_instruction(tokens, 3);
-    add_new_object(minirt, new_plane);
+    add_new_plane_object(minirt, new_plane);
 }
 
 void    initialize_sphere(t_minirt *minirt, char **tokens)
@@ -37,7 +37,7 @@ void    initialize_sphere(t_minirt *minirt, char **tokens)
     new_sphere->center = get_instruction(tokens, 1);
     new_sphere->diameter = get_instruction_double(tokens, 2);
     new_sphere->color = get_instruction(tokens, 3);
-    add_new_object(minirt, new_sphere);
+    add_new_sphere_object(minirt, new_sphere);
 }
 
 void    initialize_cylinder(t_minirt *minirt, char **tokens)
@@ -52,43 +52,43 @@ void    initialize_cylinder(t_minirt *minirt, char **tokens)
     new_cylinder->diameter = get_instruction_double(tokens, 3);
     new_cylinder->height = get_instruction_double(tokens, 4);
     new_cylinder->color = get_instruction(tokens, 5);
-    add_new_object(minirt, new_cylinder);
+    add_new_cylinder_object(minirt, new_cylinder);
 }
 
-void    initialize_ambient_light(t_minirt *minirt, char **tokens)
-{
-    t_ambient *ambient_light;
+// void    initialize_ambient_light(t_minirt *minirt, char **tokens)
+// {
+//     t_ambient *ambient_light;
 
-    ambient_light = ft_calloc(sizeof(*ambient_light), 1);
-    if (ambient_light == NULL)
-        return ;
-    ambient_light->ratio = get_instruction_double(tokens, 1);
-    ambient_light->color = get_instruction(tokens, 2);
-    minirt->scene->ambient_light = ambient_light;
-}
+//     ambient_light = ft_calloc(sizeof(*ambient_light), 1);
+//     if (ambient_light == NULL)
+//         return ;
+//     ambient_light->ratio = get_instruction_double(tokens, 1);
+//     ambient_light->color = get_instruction(tokens, 2);
+//     minirt->scene->ambient_light = ambient_light;
+// }
 
-void    initialize_camera(t_minirt *minirt, char **tokens)
-{
-    t_camera *camera;
+// void    initialize_camera(t_minirt *minirt, char **tokens)
+// {
+//     t_camera *camera;
 
-    camera = ft_calloc(sizeof(*camera), 1);
-    if (camera == NULL)
-        return ;
-    camera->view_point = get_instruction(tokens, 1);
-    camera->orientation = get_instruction(tokens, 2);
-    camera->FOV = get_instruction_double(tokens, 3);
-    minirt->scene->camera = camera;
-}
+//     camera = ft_calloc(sizeof(*camera), 1);
+//     if (camera == NULL)
+//         return ;
+//     camera->view_point = get_instruction(tokens, 1);
+//     camera->orientation = get_instruction(tokens, 2);
+//     camera->FOV = get_instruction_double(tokens, 3);
+//     minirt->scene->camera = camera;
+// }
 
-void    initialize_light(t_minirt *minirt, char **tokens)
-{
-    t_light *light;
+// void    initialize_light(t_minirt *minirt, char **tokens)
+// {
+//     t_light *light;
 
-    light = ft_calloc(sizeof(*light), 1);
-    if (light == NULL)
-        return ;
-    light->pos = get_instruction(tokens, 1);
-    light->ratio = get_instruction_double(tokens, 2);
-    light->color = get_instruction(tokens, 3);
-    minirt->scene->light = light;
-}
+//     light = ft_calloc(sizeof(*light), 1);
+//     if (light == NULL)
+//         return ;
+//     light->pos = get_instruction(tokens, 1);
+//     light->ratio = get_instruction_double(tokens, 2);
+//     light->color = get_instruction(tokens, 3);
+//     minirt->scene->light = light;
+// }
--- src/parsing/parsing_utils.c
+++ src/parsing/parsing_utils.c
@@ -6,38 +6,94 @@
 /*   By: mathia <mathia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 09:42:40 by mathia            #+#    #+#             */
-/*   Updated: 2023/04/08 20:01:42 by mathia           ###   ########.fr       */
+/*   Updated: 2023/04/09 15:28:35 by mathia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "scene.h"
 #include "parsing.h"
 
-void    add_new_object(t_minirt *minirt, void *new_object_content)
+void    add_new_plane_object(t_minirt *minirt, t_plane *new_object_content)
 {
     t_object    *temp;
     t_object    *new_object;
 
     if (!new_object_content)
         return ;
-    new_object = calloc(sizeof(*new_object), 1);
-    if (new_object == NULL)
-        return ;
     temp = minirt->scene->objects;
-    while (temp)
+    while (temp->next)
         temp = temp->next;
-    if (!temp->content)
+    if (!temp->first)
     {
-        temp->content = new_object_content;
-        free(new_object);
+        temp->plane = new_object_content;
+        temp->first = 1;
     }
     else
     {
-        temp->next = new_object;
-        new_object->content = new_object_content;
+        new_object = calloc(sizeof(*new_object), 1);
+        if (new_object == NULL)
+            return ;
+        new_object->plane = new_object_content;
+        new_object->first = 1;
         new_object->next = NULL;
+        temp->next = new_object;
     }
 }
+
+void    add_new_sphere_object(t_minirt *minirt, t_sphere *new_object_content)
+{
+    t_object    *temp;
+    t_object    *new_object;
+
+    if (!new_object_content)
+        return ;
+    temp = minirt->scene->objects;
+    while (temp->next)
+        temp = temp->next;
+    if (!temp->first)
+    {
+        temp->sphere = new_object_content;
+        temp->first = 1;
+    }
+    else
+    {
+        new_object = calloc(sizeof(*new_object), 1);
+        if (new_object == NULL)
+            return ;
+        new_object->sphere = new_object_content;
+        new_object->first = 1;
+        new_object->next = NULL;
+        temp->next = new_object;
+    }
+}
+
+void    add_new_cylinder_object(t_minirt *minirt, t_cylinder *new_object_content)
+{
+    t_object    *temp;
+    t_object    *new_object;
+
+    if (!new_object_content)
+        return ;
+    temp = minirt->scene->objects;
+    while (temp->next)
+        temp = temp->next;
+    if (!temp->first)
+    {
+        temp->cylinder = new_object_content;
+        temp->first = 1;
+    }
+    else
+    {
+        new_object = calloc(sizeof(*new_object), 1);
+        if (new_object == NULL)
+            return ;
+        new_object->cylinder = new_object_content;
+        new_object->first = 1;
+        new_object->next = NULL;
+        temp->next = new_object;
+    }
+}
+
 int    check_commas(char *token)
 {
     int i;
