0fa746beee4c9e453724785f8782fcbb2cad4380
bl000m <mathiapagani@gmail.com>
2023-04-24 14:46:16 +0200

    wip

--- src/parsing/init_ambient_instructions.c
+++ src/parsing/init_ambient_instructions.c
@@ -6,7 +6,7 @@
 /*   By: mpagani <mpagani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 09:36:05 by mathia            #+#    #+#             */
-/*   Updated: 2023/04/21 16:19:51 by mpagani          ###   ########.fr       */
+/*   Updated: 2023/04/24 14:45:32 by mpagani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,14 @@ int	initialize_ambient_light(t_minirt *minirt, char **tokens, char *line)
 		error_manager(minirt, "Error: ratio must be between 0 and 1", RED);
 	}
 	ambient_light.color = get_instruction(tokens, 2, minirt, line);
+	if (ambient_light.color < 0 || ambient_light.color > 255)
+	{
+		finish_gnl(minirt->fd);
+		closing_fd(minirt);
+		ft_free(tokens);
+		free(line);
+		error_manager(minirt, "Error: ratio must be between 0 and 1", RED);
+	}
 	minirt->scene->ambient_light = ambient_light;
 	return (error);
 }
